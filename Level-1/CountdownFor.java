8e5413892eda0379e4fd3bf8409c5a480884138e
karthikghnana <karhikghnana@gmail.com>
2026-03-06 05:20:28 +0530

    takes a number and counts to zero from the number using for loop

---
+++ Level-1/CountdownFor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CountdownFor {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter countdown number: ");
+        int n = input.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        input.close();
+    }
+}
