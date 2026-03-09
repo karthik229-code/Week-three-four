572b4b5b7f2469a890b911f3346fb8b185494599
karthikghnana <karhikghnana@gmail.com>
2026-03-10 00:27:27 +0530

    Multiples Below hundred using while

---
+++ Level-2/MultiplesWhile.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MultiplesWhile {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int counter = 100;
+
+        while(counter >= 1) {
+
+            if(counter % number == 0) {
+                System.out.println(counter);
+            }
+
+            counter--;
+        }
+
+        input.close();
+    }
+}
