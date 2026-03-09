31a6e87fe49915fc1837651687e36d659c157765
karthikghnana <karhikghnana@gmail.com>
2026-03-09 23:35:01 +0530

    adds 5% bonus if employee work exp is more than 5 years

---
+++ Level-2/Bonus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Bonus {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter salary: ");
+        double salary = input.nextDouble();
+
+        System.out.print("Enter years of service: ");
+        int years = input.nextInt();
+
+        if(years > 5) {
+            double bonus = salary * 0.05;
+            System.out.println("Bonus amount: " + bonus);
+        } 
+        else {
+            System.out.println("No bonus");
+        }
+
+        input.close();
+    }
+}
