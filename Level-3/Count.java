d8cf0e18306ad059b9e15a772bcd4c5c85130692
karthikghnana <karhikghnana@gmail.com>
2026-03-11 11:11:45 +0530

    gives number of digits in a number

---
+++ Level-3/Count.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Count {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int count = 0;
+
+        while(number != 0) {
+            number = number / 10;
+            count++;
+        }
+
+        System.out.println("Number of digits: " + count);
+
+        input.close();
+    }
+}
